c56dc964552cd8496536108bee6e5711bbd4f5f4
Екимов Вадим <ekimov_vd@mail.ru>
2021-05-27 02:44:02 +0700

    Мультиметод - а

--- src/shrubs/Shrubs.js
+++ src/shrubs/Shrubs.js
@@ -2,4 +2,5 @@ export function Shrubs({ name, id, month }) {
   this.name = name;
   this.id = id;
   this.month = month;
+  this.className = "Shrubs";
 }
--- src/trees/Trees.js
+++ src/trees/Trees.js
@@ -2,4 +2,5 @@ export function Trees({ name, id, age }) {
   this.name = name;
   this.id = id;
   this.age = age;
+  this.className = "Trees";
 }
